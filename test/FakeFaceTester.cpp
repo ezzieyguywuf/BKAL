a079b3c49f3d6e6244451be1e4ea2ca1ceee010e
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2018-02-26 16:54:20 -0500

    Added FALSE check for IFace::sharedEdge.

--- test/FakeFaceTester.cpp
+++ test/FakeFaceTester.cpp
@@ -49,18 +49,24 @@ TEST(FakeFace, NotEquals){
 
 TEST(FakeFace, SharedEdge){
     // First, create three IEdges (for a triangular face)
-    pIEdges IEdges1, IEdges2;
+    pIEdges IEdges1, IEdges2, IEdges3;
     IEdges1.push_back(std::move(pIEdge(new Edge(1))));
     IEdges1.push_back(std::move(pIEdge(new Edge(2))));
     IEdges1.push_back(std::move(pIEdge(new Edge(3))));
     IEdges2.push_back(std::move(pIEdge(new Edge(1))));
     IEdges2.push_back(std::move(pIEdge(new Edge(4))));
     IEdges2.push_back(std::move(pIEdge(new Edge(5))));
+    IEdges3.push_back(std::move(pIEdge(new Edge(6))));
+    IEdges3.push_back(std::move(pIEdge(new Edge(7))));
+    IEdges3.push_back(std::move(pIEdge(new Edge(8))));
 
     // Next, create two faces with these Edges
     pIFace face1(new Face(std::move(IEdges1)));
     pIFace face2(new Face(std::move(IEdges2)));
+    pIFace face3(new Face(std::move(IEdges3)));
 
-    // Now, assert that face1 and face2 share an Edge.
+    // Now, assert that face1 and face2 share an Edge, and thate face1 and face3 don't.
     EXPECT_TRUE(face1->sharesEdge(*face2));
+    EXPECT_FALSE(face1->sharesEdge(*face3));
+    EXPECT_FALSE(face2->sharesEdge(*face3));
 }
