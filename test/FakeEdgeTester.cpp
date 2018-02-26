fad742b126701d7d52c93436a5b47fb2cdf3bedd
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2018-02-26 15:28:40 -0500

    Updated test to use BKAL/IEdge

--- test/FakeEdgeTester.cpp
+++ test/FakeEdgeTester.cpp
@@ -1,10 +1,14 @@
 #include <BKAL/Fake/Edge.h>
+#include <BKAL/IEdge.h>
 #include "gtest/gtest.h"
 
 using Fake::Edge;
 
 TEST(FakeEdge, Equals){
-    Edge edge1(1), edge2(1), edge3(2);
-    EXPECT_EQ(edge1, edge2);
-    EXPECT_NE(edge1, edge3);
+    //Edge edge1(1), edge2(1), edge3(2);
+    IEdge* edge1 = new Edge(1);
+    IEdge* edge2 = new Edge(1);
+    IEdge* edge3 = new Edge(2);
+    EXPECT_EQ(*edge1, *edge2);
+    EXPECT_NE(*edge1, *edge3);
 }
