f03bf2f8dbf2055fcc86dbce185145c3381a9e03
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2018-02-28 12:51:20 -0500

    Removed un-necessary methods from ISolid

--- include/BKAL/ISolid.h
+++ include/BKAL/ISolid.h
@@ -13,8 +13,6 @@ class ISolid
 {
     public:
         virtual inline ~ISolid() = default;
-        inline const IEdge& getEdge(unsigned int index) const;
-        inline const IFace& getFace(unsigned int index) const;
         inline const pIFaces& getFaces() const;
         inline const pIEdges& getEdges() const;
 
@@ -22,16 +20,6 @@ class ISolid
         virtual const pIFaces& getFaceVector() const = 0;
         virtual const pIEdges& getEdgeVector() const = 0;
 };
-
-const IEdge& ISolid::getEdge(unsigned int index) const
-{
-    return *(this->getEdges()[index]);
-}
-
-const IFace& ISolid::getFace(unsigned int index) const
-{
-    return *(this->getFaces()[index]);
-}
 
 const pIFaces& ISolid::getFaces() const
 {
